65c34b29f55c37ce14f54f800c0dee7816f19d72
FabianRag5 <hector.rivera2888@alumnos.udg.mx>
2025-03-10 17:50:47 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Banner from "./components/banner/Banner";
+
+export default function App() {
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
+}
---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Banner.css";
+
+function Banner() {
+  return <div className="warning">hola</div>;
+}
+
+export default Banner;
